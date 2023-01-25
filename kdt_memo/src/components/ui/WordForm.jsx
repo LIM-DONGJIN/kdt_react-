9ebb24197d77a401adf13018e9cf22ca3d774ce4
LIM-DONGJIN <121010990+LIM-DONGJIN@users.noreply.github.com>
2023-01-25 15:20:52 +0900

    form

--- kdt_memo/src/components/ui/WordForm.jsx
+++ kdt_memo/src/components/ui/WordForm.jsx
@@ -1,27 +1,41 @@
+import React, { useState } from 'react';
+
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 
 function WordForm() {
+
+  const [word, setWord] = useState(
+    {
+      id: 0,
+      day: 0,
+      eng: "",
+      kor: "",
+      isDone: false
+    }
+  );
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  }
+
   return (
-    <Form>
-      <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Label>Email address</Form.Label>
-        <Form.Control type="email" placeholder="Enter email" />
-        <Form.Text className="text-muted">
-          We'll never share your email with anyone else.
-        </Form.Text>
+    <Form onSubmit={handleSubmit}>
+
+      <Form.Group className="mb-3">
+        <Form.Label>English Word</Form.Label>
+        <Form.Control type="text" placeholder="영어 단어를 입력해 주세요." />
       </Form.Group>
 
-      <Form.Group className="mb-3" controlId="formBasicPassword">
-        <Form.Label>Password</Form.Label>
-        <Form.Control type="password" placeholder="Password" />
+      <Form.Group className="mb-3">
+        <Form.Label>Korean Word</Form.Label>
+        <Form.Control type="text" placeholder="한국어 단어를 입력해 주세요." />
       </Form.Group>
-      <Form.Group className="mb-3" controlId="formBasicCheckbox">
-        <Form.Check type="checkbox" label="Check me out" />
-      </Form.Group>
+
       <Button variant="primary" type="submit">
         Submit
       </Button>
+
     </Form>
   );
 }
