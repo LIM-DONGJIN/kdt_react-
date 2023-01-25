936628522120c2665af7e0cce7b4a482743c8e3d
LIM-DONGJIN <121010990+LIM-DONGJIN@users.noreply.github.com>
2023-01-25 17:37:08 +0900

    module.css

--- kdt_shop/src/components/layout/Header.jsx
+++ kdt_shop/src/components/layout/Header.jsx
@@ -1,8 +1,20 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import styles from './Header.module.css';
 
 function Header() {
   return ( 
-    <>header</>
+    <header>
+      <h1 className={styles.logo}>SSG.com</h1>
+      <nav>
+        <ul>
+          <li><Link to='/'>Main</Link></li>
+          <li><Link to='/product'>ProductList</Link></li>
+          <li><Link to='/cart'>Cart</Link></li>
+          <li><Link to='/wish'>Wish</Link></li>
+        </ul>
+      </nav>
+    </header>
    );
 }
 
