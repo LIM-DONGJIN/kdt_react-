aa6164f9181c1de5736db5de0c35f32f89411e35
LIM-DONGJIN <121010990+LIM-DONGJIN@users.noreply.github.com>
2023-01-25 13:24:11 +0900

    fetch and get

--- kdt_memo/src/components/ui/MainTable.jsx
+++ kdt_memo/src/components/ui/MainTable.jsx
@@ -1,12 +1,24 @@
-import React, { useState } from 'react';
+import React from 'react';
+import { useState } from 'react';
+import { useEffect } from 'react';
 
 import Table from 'react-bootstrap/Table';
-
-import data from '../../db/data.json';
+// import data from '../../db/data.json';
 import WordList from './WordList';
 
 function MainTable() {
 
+  const [data, setData] = useState();
+  
+  useEffect(()=>{
+    fetch('http://localhost:3001/words')
+    .then(res=>res.json())
+    .then(data => {
+      console.log(data);
+      setData(data)
+    })
+    .catch(err=>console.log(err));
+  },[]);
 
   return (
     <Table striped bordered hover>
@@ -21,7 +33,7 @@ function MainTable() {
       </thead>
       <tbody>
         {
-          data.words.map(word=>(
+          data && data.map(word=>(
             <WordList
               key={word.id}
               word={word}
