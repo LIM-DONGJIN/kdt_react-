ddde40ff5a9904ded60a5edef89a088039ce2cb8
LIM-DONGJIN <121010990+LIM-DONGJIN@users.noreply.github.com>
2023-01-25 15:12:53 +0900

    word form

--- kdt_memo/src/components/pages/CreateWord.jsx
+++ kdt_memo/src/components/pages/CreateWord.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import WordForm from '../ui/WordForm';
 
 function CreateWord() {
   return ( 
-    <>
+    <div className='container mt-5'>
       <h2>Create Word page</h2>
-    </>
+      <WordForm />
+    </div>
    );
 }
 
